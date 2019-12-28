a985609fa9b378b72537a44c355817c8822ab251
kohill <kohillyang@126.com>
2019-12-28 22:18:18 +0800

    pub imu data

--- h007/VINS-Course/test/run_euroc.cpp
+++ h007/VINS-Course/test/run_euroc.cpp
@@ -6,7 +6,10 @@
 #include <iostream>
 #include <thread>
 #include <iomanip>
-
+#include <QString>
+#include <QFile>
+#include <QIODevice>
+#include <QTextStream>
 #include <cv.h>
 #include <opencv2/opencv.hpp>
 #include <highgui.h>
@@ -19,75 +22,38 @@ using namespace Eigen;
 
 const int nDelayTimes = 2;
 string sData_path = "/home/dataset/EuRoC/MH-05/mav0/";
-string sConfig_path = "../config/";
+string sConfig_path = "/data3/zyx/yks/vio_homeworks/h007/VINS-Course/config/";
 
 std::shared_ptr<System> pSystem;
 
 void PubImuData()
 {
-	string sImu_data_file = sConfig_path + "MH_05_imu0.txt";
-	cout << "1 PubImuData start sImu_data_filea: " << sImu_data_file << endl;
-	ifstream fsImu;
-	fsImu.open(sImu_data_file.c_str());
-	if (!fsImu.is_open())
-	{
-		cerr << "Failed to open imu file! " << sImu_data_file << endl;
-		return;
-	}
-
-	std::string sImu_line;
-	double dStampNSec = 0.0;
-	Vector3d vAcc;
-	Vector3d vGyr;
-	while (std::getline(fsImu, sImu_line) && !sImu_line.empty()) // read imu data
-	{
-		std::istringstream ssImuData(sImu_line);
-		ssImuData >> dStampNSec >> vGyr.x() >> vGyr.y() >> vGyr.z() >> vAcc.x() >> vAcc.y() >> vAcc.z();
-		// cout << "Imu t: " << fixed << dStampNSec << " gyr: " << vGyr.transpose() << " acc: " << vAcc.transpose() << endl;
+    const QString imu_path = "/data3/zyx/yks/vio_homeworks/h007/vio_data_simulation/bin/imu_pose.txt";
+    QFile imu_file(imu_path);
+    if (!imu_file.open(QIODevice::ReadOnly | QIODevice::Text)){
+        std::exit(-1);
+    }
+    QTextStream imu_stream(&imu_file);
+    // timestamp (1)，imu quaternion(4)，imu position(3)，imu gyro(3)，imu acc(3)
+	QString line = imu_stream.readLine();
+	while (!line.isNull()) // read imu data
+	{   QStringList line_split = line.split(" ");
+	    Q_ASSERT(line_split.size() == 14);
+	    double dStampNSec = line_split[0].toDouble();
+        Vector3d vAcc = Vector3d(line_split[11].toDouble(),line_split[12].toDouble(),line_split[13].toDouble());
+        Vector3d vGyr = Vector3d(line_split[8].toDouble(),line_split[9].toDouble(),line_split[10].toDouble());
 		pSystem->PubImuData(dStampNSec / 1e9, vGyr, vAcc);
 		usleep(5000*nDelayTimes);
 	}
-	fsImu.close();
+    imu_file.close();
 }
 
 void PubImageData()
 {
-	string sImage_file = sConfig_path + "MH_05_cam0.txt";
-
-	cout << "1 PubImageData start sImage_file: " << sImage_file << endl;
-
-	ifstream fsImage;
-	fsImage.open(sImage_file.c_str());
-	if (!fsImage.is_open())
-	{
-		cerr << "Failed to open image file! " << sImage_file << endl;
-		return;
-	}
-
-	std::string sImage_line;
-	double dStampNSec;
-	string sImgFileName;
-	
-	// cv::namedWindow("SOURCE IMAGE", CV_WINDOW_AUTOSIZE);
 	while (std::getline(fsImage, sImage_line) && !sImage_line.empty())
 	{
-		std::istringstream ssImuData(sImage_line);
-		ssImuData >> dStampNSec >> sImgFileName;
-		// cout << "Image t : " << fixed << dStampNSec << " Name: " << sImgFileName << endl;
-		string imagePath = sData_path + "cam0/data/" + sImgFileName;
-
-		Mat img = imread(imagePath.c_str(), 0);
-		if (img.empty())
-		{
-			cerr << "image is empty! path: " << imagePath << endl;
-			return;
-		}
 		pSystem->PubImageData(dStampNSec / 1e9, img);
-		// cv::imshow("SOURCE IMAGE", img);
-		// cv::waitKey(0);
-		usleep(50000*nDelayTimes);
 	}
-	fsImage.close();
 }
 
 #ifdef __APPLE__
@@ -149,14 +115,14 @@ void DrawIMGandGLinMainThrd(){
 
 int main(int argc, char **argv)
 {
-	if(argc != 3)
-	{
-		cerr << "./run_euroc PATH_TO_FOLDER/MH-05/mav0 PATH_TO_CONFIG/config \n" 
-			<< "For example: ./run_euroc /home/stevencui/dataset/EuRoC/MH-05/mav0/ ../config/"<< endl;
-		return -1;
-	}
-	sData_path = argv[1];
-	sConfig_path = argv[2];
+//	if(argc != 3)
+//	{
+//		cerr << "./run_euroc PATH_TO_FOLDER/MH-05/mav0 PATH_TO_CONFIG/config \n"
+//			<< "For example: ./run_euroc /home/stevencui/dataset/EuRoC/MH-05/mav0/ ../config/"<< endl;
+//		return -1;
+//	}
+//	sData_path = argv[1];
+//	sConfig_path = argv[2];
 
 	pSystem.reset(new System(sConfig_path));
 	
@@ -168,7 +134,7 @@ int main(int argc, char **argv)
 	std::thread thd_PubImageData(PubImageData);
 
 #ifdef __linux__	
-	std::thread thd_Draw(&System::Draw, pSystem);
+//	std::thread thd_Draw(&System::Draw, pSystem);
 #elif __APPLE__
 	DrawIMGandGLinMainThrd();
 #endif
